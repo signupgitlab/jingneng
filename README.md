ac1e5eab24716b36b7568e6a7e2a40f2f6e63d6d
zhangli <zhangli03@chinacoal.com>
2020-07-09 20:51:58 +0800

    项目调整

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# my-project
+# jingneng
 
 ## Project setup
 ```
---
+++ src/views/homePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home_page">
+    asdfsdf
+  </div>
+</template>
+<script>
+export default {
+  name: 'homePage',
+}
+</script>
+<style lang="scss">
+.home_page {
+  
+}
+</style>
